485561d0509bcc93b5ab6b731e1a57d28f9ef949
Yale Connect <104935762+YaleConnect@users.noreply.github.com>
2025-01-24 19:47:40 -0300

    Update definitions.ts addlistener devuelve buffer del tipo number[]

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,34 +1,41 @@
 import type { PluginListenerHandle } from '@capacitor/core';
-
 export interface UDPPlugin {
   create(options?: {
-    properties?: { name?: string; bufferSize?: number };
-  }): Promise<{ socketId: number; ipv4: string; ipv6: string }>;
-
+    properties?: {
+      name?: string;
+      bufferSize?: number;
+    };
+  }): Promise<{
+    socketId: number;
+    ipv4: string;
+    ipv6: string;
+  }>;
   update(options: {
     socketId: number;
-    properties: { name?: string; bufferSize?: number };
+    properties: {
+      name?: string;
+      bufferSize?: number;
+    };
   }): Promise<any>;
-
-  setPaused(options: { socketId: number; paused: boolean }): Promise<any>;
-
+  setPaused(options: {
+    socketId: number;
+    paused: boolean;
+  }): Promise<any>;
   bind(options: {
     socketId: number;
     address: string;
     port: number;
   }): Promise<any>;
-
   send(options: {
     socketId: number;
     address: string;
     port: number;
-    buffer: string;
+    buffer: number[];
   }): Promise<any>;
-
   closeAllSockets(): Promise<any>;
-
-  close(options: { socketId: number }): Promise<any>;
-
+  close(options: {
+    socketId: number;
+  }): Promise<any>;
   getInfo(options: {
     socketId: number;
   }): Promise<{
@@ -39,7 +46,6 @@ export interface UDPPlugin {
     localAddress?: string;
     localPort?: number;
   }>;
-
   getSockets(): Promise<{
     sockets: [
       {
@@ -49,42 +55,35 @@ export interface UDPPlugin {
         paused: boolean;
         localAddress?: string;
         localPort?: number;
-      },
+      }
     ];
   }>;
-
-  joinGroup(options: { socketId: number; address: string }): Promise<any>;
-
-  leaveGroup(options: { socketId: number; address: string }): Promise<any>;
-
+  joinGroup(options: {
+    socketId: number;
+    address: string;
+  }): Promise<any>;
+  leaveGroup(options: {
+    socketId: number;
+    address: string;
+  }): Promise<any>;
   setMulticastTimeToLive(options: {
     socketId: number;
     ttl: number;
   }): Promise<any>;
-
-  setBroadcast(options: { socketId: number; enabled: boolean }): Promise<any>;
-
+  setBroadcast(options: {
+    socketId: number;
+    enabled: boolean;
+  }): Promise<any>;
   setMulticastLoopbackMode(options: {
     socketId: number;
     enabled: boolean;
   }): Promise<any>;
-
-  getJoinedGroups(): Promise<{ groups: [string] }>;
-
-  addListener(
-    events: 'receive',
-    functions: (params: { socketId: number; buffer: string }) => void,
-  ): PluginListenerHandle;
-
-  addListener(
-    events: 'receiveError',
-    functions: (params: UdpReceiveError) => void,
-  ): PluginListenerHandle;
+  getJoinedGroups(): Promise<{
+    groups: [string];
+  }>;
+  addListener(events: 'receive', functions: (params: {
+    socketId: number;
+    buffer: number[];
+  }) => void): PluginListenerHandle;
+  addListener(events: 'receiveError', functions: (params: string) => void): PluginListenerHandle;
 }
-
-
-
-export type UdpReceiveError = {
-  code: number;
-  message: string;
-};
